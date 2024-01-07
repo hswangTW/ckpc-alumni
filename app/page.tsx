e502dd0d77e00d1e87815bc8569fc13a06cb1a8e
Han-Sheng Wang <hanson2693@gmail.com>
2024-01-07 23:22:22 +0800

    feat: :lipstick: add index page; update style

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Link from 'next/link';
+
+export default function Home() {
+  return (
+    <main>
+      <p>Coming soon...</p>
+      <Link href='/ckpc31' className='text-sky-400'>
+        Go to CKPC 31st Anniversary
+      </Link>
+    </main>
+  )
+}
